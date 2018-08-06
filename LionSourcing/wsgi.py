591f7f5e5517906fe7e83d31dd37d6f53bdaa937
pajuhy123 <pajuhy123@gmail.com>
2018-08-06 16:45:23 +0900

    구동환경 settings 분기

--- LionSourcing/wsgi.py
+++ LionSourcing/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LionSourcing.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LionSourcing.settings.prod_aws_eb")
 
 application = get_wsgi_application()
